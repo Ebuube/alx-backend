b937b882c1689ab24cf73414df4b30d0dd5964a7
Ebuube <onwutaebubegideon1555@gmail.com>
2024-02-21 22:08:07 +0000

    In development: needs to correct eviction algorithm - pop

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""LRU Caching
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """A caching class that implements Least Recently Used caching policy
+    """
+    order = {}
+
+    def put(self, key, item):
+        """Insert an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if (len(self.cache_data) == BaseCaching.MAX_ITEMS and
+                key not in self.cache_data):
+            discarded = self.pop()
+            print("DISCARD: {}".format(discarded[0]))
+
+        self.cache_data[key] = item
+        self.ping(key)
+
+    def pop(self):
+        """Remove the least recently used key and item
+        """
+        min_used = None
+        equal_use = True
+        for key, val in self.order.items():
+            if not min_used:
+                min_used = key
+                continue
+
+            if val != self.order[min_used]:
+                equal_use = False
+            if val < self.order[min_used]:
+                min_used = key
+
+        # If equally used, use FIFO to evict cache block
+        if equal_use:
+            for key, val in self.order.items():
+                min_used = key
+                break
+
+        item = self.cache_data.pop(min_used)
+
+        # update order
+        self.order.pop(min_used)
+        for key in self.order:
+            self.order[key] = self.order[key] - 1
+
+        return min_used, item
+
+    def ping(self, key):
+        """Update use-count for a key
+        """
+        if key not in self.order:
+            self.order[key] = 1
+            return
+
+        self.order[key] = self.order[key] + 1
+
+    def get(self, key):
+        """Fetch the specified data from the cache
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
--- 0x01-caching/test.py
+++ 0x01-caching/test.py
@@ -7,3 +7,4 @@ Testing resources
 BasicCache = __import__('0-basic_cache').BasicCache
 FIFOCache = __import__('1-fifo_cache').FIFOCache
 LIFOCache = __import__('2-lifo_cache').LIFOCache
+LRUCache = __import__('3-lru_cache').LRUCache
