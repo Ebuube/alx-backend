e62c654c1725473a3a9ca858bf6ced6454c0a6fc
Ebuube <onwutaebubegideon1555@gmail.com>
2024-02-29 05:55:24 +0000

    Modified files a bit

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,11 +31,14 @@ def home() -> str:
     return render_template('2-index.html')
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale():
     """Get the locale for a given web page
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 if __name__ == '__main__':
